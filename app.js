e1f663f19ac1a5d79797da91a19390b38a5a7410
Melek touihri <twihrimelek9@gmail.com>
2024-07-06 14:10:31 +0100

    Color Background Change With HTML, CSS And JS

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+function change_color(color){
+    body =  document.getElementById('a').style.background = color;
+};
+
+function text (){
+    document.getElementById('title').style.color = "brown";
+}
